56ce7d3ca5bb7c8d2d32f1849c0dbab8d496098b
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2018-12-21 22:50:36 -0500

    Greatly simplified

--- ch5/ex5-5.lua
+++ ch5/ex5-5.lua
@@ -5,7 +5,6 @@
 n additions and n multiplications (and no exponentiations?)
 ]]
 
--- tbc
 function print_poly (poly)
 	for _, term in ipairs(poly) do
 		io.write(term, " ")
@@ -13,42 +12,17 @@ function print_poly (poly)
 	io.write("\n")
 end
 
-function compute_poly (poly, x)
+function compute_poly(poly, x)
+	local sum = 0
 
-	-- This set of recursive calls must use a local copy of POLY,
-	-- or else future computations of COMPUTE_POLY will be affected,
-	-- as the global copy of POLY will have been modified destructively.
-	local poly = table.move(poly, 1, #poly, 1, {})
-
-	-- Count the number of times the recursive case of COMPUTE_POLY1 is called,
-	-- to prove that COMPUTE_POLY uses at most n additions and
-	-- n multiplications.
-	local cnt = 0
-
-	-- The actual recursive function to compute the polynomial at x
-	local function compute_poly1 (poly)
-
-
-		local degree, first_term = #poly - 1, poly[1]
-
-		if degree == 0 then
-			return first_term
-		else
-			-- record the recursive call to COMPUTE_POLY1
-			cnt = cnt + 1
-
-			-- Obtain the shifted polynomial
-			table.move(poly, 2, #poly, 1)
-			poly[#poly] = nil
-
-			return first_term + x * compute_poly1(poly, x)
-		end
+	for i = #poly, 1, -1 do
+		sum = sum * x + poly[i]
 	end
 
-	-- Note that we also return the final value of CNT.
-	return compute_poly1(poly), cnt
+	return sum
 end
 
+-- TESTS
 examples = {
 	{1, 1, 1},  -- x2 + x + 1
 	{1, 0, 1},  -- x2 + 1
@@ -65,14 +39,7 @@ end
 function run_tests ()
 	for num, poly in ipairs(examples) do
 		for i = 0, 2 do
-			local degree = #poly - 1
-			local ans, cnt = compute_poly(poly, i)
-
-			if cnt > degree then
-				io.write(string.format("Degree: %d, Count: %d; Example: %d, Val_x: %d\n",
-										degree, cnt, num, i))
-
-			end
+			local ans = compute_poly(poly, i)
 
 			if num == 1 then
 				if i == 0 then
@@ -117,4 +84,8 @@ function run_tests ()
 			end
 		end
 	end
+
+	print("All assertions passed!")
 end
+
+run_tests()
