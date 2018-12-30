581d568596dab55b0d809e5f5910acf7496aed84
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2018-12-29 22:58:46 -0500

    Started chapter 11

---
+++ ch11/word_count.lua
@@ -0,0 +1,32 @@
+--[[
+	Listing 11.1
+
+	Word frequency program
+]]
+
+local counter = {}
+
+for line in io.lines() do
+	for word in string.gmatch(line, "%w+") do
+		counter[word] = (counter[word] or 0) + 1
+	end
+end
+
+local words = {} -- sorting needs a proper list
+
+for w in pairs(counter) do
+	words[#words + 1] = w -- load 'words' with the keys from 'counter'
+end
+
+table.sort(words, function (w1, w2)
+	return counter[w1] > counter[w2] or
+		counter[w1] == counter[w2] and w1 < w2
+end)
+
+-- number of words to print
+local n = math.min(tonumber(arg[1]) or math.huge, #words) -- clever :)
+
+for i = 1, n do
+	io.write(words[i], "\t", counter[words[i]], "\n")
+end
+
