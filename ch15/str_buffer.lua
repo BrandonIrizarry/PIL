19d9b68a641b2065eb51a401f6459d184f7e4265
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-01-31 19:20:36 -0500

    Got string buffer module to work

--- ch15/str_buffer.lua
+++ ch15/str_buffer.lua
@@ -1,35 +1,17 @@
 
 local M = {}
 
-function M.str_buffer ()
-	local result = {}
-	
-	return {
-		add = 
-		function (fmt, ...)
-			result[#result + 1] = string.format(fmt, ...)
-		end,
-		
-		fetch =
-		function ()
-			local str = table.concat(result)
-			result = {}
-			return str
-		end,
-	}
+
+local buffer = {}
+
+function M.add (fmt, ...)
+	buffer[#buffer + 1] = string.format(fmt, ...)
 end
 
-
-local function tests ()
-	B = str_buffer()
-	B.add("{\n\t%s", "tree")
-	B.add("\n\t%s", "house")
-	s = B.fetch()
-	print(s)
-
-	B.add("%d", 5)
-	r = B.fetch()
-	print(r)
+function M.flush ()
+	local str = table.concat(buffer)
+	buffer = {}
+	return str
 end
 
 return M
