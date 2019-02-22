a18793e18b4c69689fcbfbc018802a0c535434b7
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-02-21 20:16:03 -0500

    Finished this example

--- ch20/examples/proxy-client.lua
+++ ch20/examples/proxy-client.lua
@@ -1,2 +1,20 @@
 local tracker = require "proxy"
 
+local track = tracker.track
+
+local t = {} -- we'll track additions and access to this table.
+
+t = track(t) -- cleverly substitute t with its proxy.
+
+t[2] = "hello"
+print(t[2])
+
+local q = {} -- what if we try to track another table?
+
+q = track(q)
+
+q[2] = "good morning"
+
+for k,v in pairs(t) do
+	print(k,v)
+end
