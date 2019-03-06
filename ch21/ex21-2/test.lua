01b5e267aecbe399bb29465a9e83084e4ac6591e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-03-05 21:29:27 -0500

    Finished ex21-2

--- ch21/ex21-2/test.lua
+++ ch21/ex21-2/test.lua
@@ -1,3 +1,23 @@
 StackQueue = require "StackQueue"
 
 sq = StackQueue:new()
+
+sq:push("A")
+print(sq)
+sq:push("B")
+sq:push("C")
+print(sq)
+sq:insertbottom("[-c]")
+sq:insertbottom("[-b]")
+sq:insertbottom("[-a]")
+print(sq)
+sq:pop()
+sq:pop()
+sq:pop()
+print(sq)
+sq:pop()
+sq:pop()
+print(sq)
+sq:insertbottom("D")
+print(sq)
+-- ... and so on.
