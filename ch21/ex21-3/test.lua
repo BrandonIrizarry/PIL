da4848a0f1c0e2c7c2f9c9504c44fa73f78aad35
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-03-08 21:26:32 -0500

    Removed beginning comment

--- ch21/ex21-3/test.lua
+++ ch21/ex21-3/test.lua
@@ -2,9 +2,6 @@
 -- Therefore, all methods are accessed via the metatable (the Stack prototype),
 -- and can't be altered on 's'.
 
--- I should've probably included 'contents' in the prototype, which would've
--- allowed me to use the original Stack (it shouldn't be considered something
--- abstract.) I'll keep this in mind for later classes/prototypes.
 s = require "Stack" 
 
 print(s.contents) -- is now nil!
