e06700dad7a06c66b47c9d1e3db80f64dec049bc
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-03-19 22:25:38 -0400

    Tweaked to learn about default values using memoized metatables that are weak values in the memo(r)izing table

--- ch23/examples/default_metas.lua
+++ ch23/examples/default_metas.lua
@@ -1,12 +1,33 @@
-local mtS = setmetatable({}, {__mode = "v"})
 
-function set_default (table_a, defval)
-	local redirect = mtS[defval]
+
+local metas = {}
+setmetatable(metas, {__mode = "v"})
+
+function setDefault (t, d)
+	local mt = metas[d]
 	
-	if redirect == nil then
-		redirect = {__index = function () return defval end}
-		mtS[defval] = redirect -- memorize/cache the value
+	if mt == nil then
+		mt = {__index = function () return d end}
+		metas[d] = mt -- memorize
 	end
 	
-	setmetatable(table_a, redirect)
+	setmetatable(t, mt)
+end
+
+local t1 = {doe = "a deer", re = "a drop", mi = "a name"}
+local t2 = {apple = "fruit", corn = "vegetable"}
+local t3 = {new_york = "albany", pennsylvania = "philadelphia"}
+
+setDefault(t1, "I don't know")
+setDefault(t2, "I don't know")
+setDefault(t3, "Springfield")
+--t1 = nil
+--t2 = nil
+
+-- When I delete the main tables, their metatables are no longer in use,
+-- and so get garbage collected (because they are weak values).
+collectgarbage()
+
+for k,v in pairs(metas) do
+	print(k,v)
 end
