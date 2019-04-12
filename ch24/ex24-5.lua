eb443ffa8ae00edad7cfc93e4b8e3ed74e022c7a
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-04-12 06:24:43 -0400

    Working on generalizing number of inputs for 'run' function

--- ch24/ex24-5.lua
+++ ch24/ex24-5.lua
@@ -7,6 +7,7 @@ threads concurrently? What would you have to change?
 
 local lib = require "examples.async-lib"
 
+--[[
 function run (code1, code2)
 	local co1 = coroutine.create(function ()
 		code1()
@@ -15,24 +16,43 @@ function run (code1, code2)
 	
 	local co2 = coroutine.create(function ()
 		code2()
-		lib.stop()
 	end)
 	
-	-- FIXME: when there's an odd number of lines, there's a problem - 
-	-- one of the lines is omitted. We should trace the execution of this
-	-- double-coroutine program, and see what would happen with three,
-	-- and get a vararg version going, and run that to see what happens, tbc.
 	coroutine.resume(co1)
 	coroutine.resume(co2)
 	lib.runloop()
 end
-
 
 function putline (stream, line)
 	local co = coroutine.running()
 	local callback = function () coroutine.resume(co) end
 	lib.writeline(stream, line, callback)
 	coroutine.yield()
+end
+--]]
+
+-- FIXME: This general version doesn't work - find the misstep. tbc.
+function run (code1, ...)
+	local co1 = coroutine.create(function ()
+		code1()
+		lib.stop()
+	end)
+	
+
+	local cbodies = table.pack(...)
+	local co_s = {}
+	
+	for i = 1, cbodies.n  do
+		table.insert(co_s, coroutine.create(function () cbodies[i]() end))
+	end
+	
+	coroutine.resume(co1)
+	
+	for _, co in ipairs(co_s) do
+		coroutine.resume(co)
+	end
+	
+	lib.runloop()
 end
 
 function getline (stream)
@@ -49,9 +69,9 @@ function lines (stream)
 	end
 end
 
-function reverse_lines ()
+function reverse_lines (inp)
 	local t = {}
-	local inp = io.input()
+	inp = inp or io.stdin
 	local out = io.output()
 	
 	for line in lines(inp) do
@@ -77,4 +97,7 @@ function label_lines ()
 	end
 end
 
-run(reverse_lines, label_lines)
+-- Try to use something different from stdin for the input stream here.
+fstream = io.open("examples/fragment.lua")
+
+run(function () reverse_lines(fstream) end, label_lines)
