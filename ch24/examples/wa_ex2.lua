fb61ea06b5900db704adb2d032e96f01faaa4f58
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-04-27 22:20:19 -0400

    Finished the second wa example, illustrating the simple use of coroutines

---
+++ ch24/examples/wa_ex2.lua
@@ -0,0 +1,25 @@
+routines = {}
+
+routines.doOnce = function (phrase)
+	print(phrase)
+end
+
+routines.doLoop = function (num_iterations)
+	for i = 1, num_iterations do
+		print("Iteration: ", i)
+		coroutine.yield()
+	end
+end
+
+routines.doTextChar = function (phrase)
+	local nchars = #phrase
+	
+	for i = 1, nchars do
+		print(phrase:sub(i,i))
+		coroutine.yield() -- note that these, for now, are 'empty yields'
+	end
+end
+
+doOnceT = coroutine.create(routines.doOnce)
+doLoopT = coroutine.create(routines.doLoop)
+doTextCharT = coroutine.create(routines.doTextChar)
