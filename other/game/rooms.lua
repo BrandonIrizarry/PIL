96f20c39ca2533f78854975114c8753f55363321
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-05-11 08:29:19 -0400

    Finished a small game

--- other/game/rooms.lua
+++ other/game/rooms.lua
@@ -1,46 +1,138 @@
-function throne_room ()
-	local has_ruby = false
+--[[
+	Implement something in the spirit of Learn Python the Hard Way by Zed Shaw -
+a simple game where we traverse some rooms, and complete some mission to win the
+game.
+	The goal is, eventually, to see if implementing the game using any sort of
+coroutine discipline (preferably symmetric) simplifies things, or contributes
+anything to the logic (e.g., lets our game use more game-play features.)
+]]
+
+Room = {}
+
+-- Let 'items' be a Lua set.
+function Room:new (items)
+
+	self.__index = self
+	return setmetatable(items or {}, self)
+end
+
+
+ThroneRoom = Room:new()
+DoorRoom = Room:new()
+KeyRoom = Room:new({key=true})
+RubyRoom = Room:new({ruby=true})
+
+function query (limit)
+	assert((math.type(limit) == "integer") and (limit > 0))
+		
+	local ans
+
+	repeat
+		print("What do you do?")
+		ans = math.tointeger(io.read())
+		
+		-- Type '0' at any prompt to exit the game.
+		if ans == 0 then os.exit() end
+		
+	until (ans ~= nil) and (ans > 0) and (ans <= limit) 
 	
-	while true do
-		print([[
+	return ans 
+end
+
+function ThroneRoom:action ()
+	print([[
 The king says, "Find me a ruby in these rooms, and you will be free."
 You see stairs that lead to another room.
-	1. Give the king the ruby.
-	2. Go down the stairs.]])
+1. Give the king the ruby.
+2. Go down the stairs.]])
 
-		repeat
-			print("What do you do (1,2)")
-			local ans = io.read("n")
-		until (ans == 1) or (ans == 2)
-		
-		if ans == 1 then 
-			transfer(win_game, has_ruby)
+	local ans = query(2)
+	
+	if ans == 1 then 
+		if RubyRoom.ruby then
+			print("You don't have the ruby yet! Keep looking.")
+			self:action() -- redo the room.
 		else
-			has_ruby = transfer(door_room)
+			print("Congratulations, you won!")
 		end
+	else
+		DoorRoom:action()
 	end
 end
 
-function win_game (has_ruby)
-	while true do
-		print("So, you say you have the ruby...")
-		if has_ruby then
-			print("Congratulations, you win! You are free.")
-			
+function DoorRoom:action ()
+	print([[
+There is a locked door in this room.
+There is also an unrestricted passage to another room.
+1. Open the door.
+2. Go through the passage.
+3. Go back to the throne room.]])
+
+	local ans = query(3)
+	
+	if ans == 1 then
+		if not KeyRoom.key then
+			print("You unlock the door.")
+			RubyRoom:action()
 		else
-			transfer(throne_room)
+			print("You don't have the key yet.")
+			self:action()
 		end
-	
+	elseif ans == 2 then
+		print("You decide to go through the passage.")
+		KeyRoom:action()
+	else
+		print("You head back to the throne room.")
+		if RubyRoom.ruby then
+			print("Are you sure you aren't missing something?")
+		end
+		ThroneRoom:action()
+	end
+end
 
-function door_room ()
-	while true do
-		print([[
-You see a locked door, and another, unrestricted passage]])
-
-	repeat
-		print("Go through the passage (1) ?")
-		local ans = io.read("1")
-	until ans == "y"
+function KeyRoom:action ()
+	print([[
+You see a box in the middle of the room. It appears to be open.
+1. Open the box
+2. Go back through the passage.]])
 	
-
+	local ans = query(2)
 	
+	if ans == 1 then
+		if self.key then
+			print("You see a key, and take it.")
+			self.key = false
+			self:action()
+		else
+			print("The box is empty.")
+			self:action()
+		end
+	else
+		DoorRoom:action()
+	end
+end
+
+function RubyRoom:action ()
+	print([[
+You see a box in the middle of the room. It appears to be open.
+1. Open the box
+2. Go back through the door.]])
+
+	local ans = query(2)
+	
+	if ans == 1 then
+		if self.ruby then 
+			print("You see a ruby, and take it.")
+			self.ruby = false
+			self:action()
+		else
+			print("The box is empty.")
+			self:action()
+		end
+	else
+		DoorRoom:action()
+	end
+end
+
+ThroneRoom:action()
+
