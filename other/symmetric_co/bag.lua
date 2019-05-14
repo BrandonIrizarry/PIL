8637b04a2afb0ea4a5150ad6f5a619cf66540604
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-05-13 22:31:08 -0400

    Started a Bag implementation

---
+++ other/symmetric_co/bag.lua
@@ -0,0 +1,16 @@
+local Bag = {collection = {}}
+
+function Bag:new (seq)
+end
+
+function Bag:add (item)
+end
+
+function Bag:remove (item)
+end
+
+function Bag:is_empty ()
+end
+
+function Bag:obj_print ()
+end
