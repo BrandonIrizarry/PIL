d5c49f11878811b84cd0d4f756611c00dd46ff6f
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-08-11 09:53:11 -0400

    Fixed comments, added caching to the environment

--- ch25/ex25-4.lua
+++ ch25/ex25-4.lua
@@ -6,25 +6,33 @@ if they were in the lexical scope of the calling function. (Hint: run the comman
 in an empty environment and use the __index metamethod attached to the function
 'getvarvalue' to do accesses to variables.
 
-	It looks like, to use getvarvalue, you need to know the variable name you're looking up,
-_outside_ of '__index', and make the call there - but you can't do that with ex25-1, so you
-need ex25-3, to get everything in a table and look up what you need there.
+	To use getvarvalue, you need to know the variable name ('word') you're looking up,
+_outside_ of '__index' (because of scoping), and make the call there - but you can't do that with 
+the original version of 'getvarvalue' from ex25-1, so you need the one completed in ex25-3, 
+to compile everything at once in a table and look up what you need there.
+	However, in that case the name 'getvarvalue' is slightly misleading, so I call it 'gvv'.
+	
+	The following is useful for debugging:
+	
+	local env = setmetatable({}, {__index = var_t})
+	
+	Then, for example, to invoke 'print(x + y)', you would type:
+	
+	globals.print(locals[1].x + locals[1].y)
 ]]
 
-local gvv = require "ex25-3"
-local getvarvalue = require "ex25-1"
+local gvv = require "ex25-3" -- 'getvarvalue', but compiles a table
 
 function debug_lex (co, chunk_name)
 	local var_t = gvv(co, 2)
 
-	-- NB: We're only looking at variables in the current scope, '1'.
-	local env = setmetatable({}, {__index = function (_, word)
-		return var_t.locals[1][word] or var_t.upvalues[1][word] or var_t.globals[word]
+	-- NB: We're only looking at variables in the current scope, that is, level 1.
+	local env = setmetatable({}, {__index = function (E, word)
+		local value = var_t.locals[1][word] or var_t.upvalues[1][word] or var_t.globals[word]
+		E[word] = value -- cache the variable name in the environment
+		
+		return value
 	end})
-
-	-- This is cool for when we need to debug for whatever reason, e.g.:
-	-- 'globals.print(locals[1].x + locals[1].y)' for 'print(x + y)'.
-	--local env = setmetatable({}, {__index = var_t})
 		
 	while true do
 		io.write(string.format("debug:%s> ", chunk_name))
