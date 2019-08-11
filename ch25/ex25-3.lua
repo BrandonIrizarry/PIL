73a44d76e85918f5f4c99e38ee21f918be504388
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-08-10 22:35:44 -0400

    Fixed t_all/getvarvalue_table to work at different levels, and moved its tests to a different file

--- ch25/ex25-3.lua
+++ ch25/ex25-3.lua
@@ -15,14 +15,9 @@ with the level as seen by Lua (e.g., 2 instead of 1).
 
 local locals = require "examples.local_it"
 local upvalues = require "examples.upvalue_it"
-local x = "the higher version of x"
 
-local nice = "foo"
-
-MESSAGE	= "I can be seen"
-
-function t_all (co)
-	local flevel = (co and 0) or 1
+return function (co, flevel)
+	local flevel = (co and 0) or flevel
 	local env -- for finding the _ENV to use with globals metatable
 	
 	local result = {}
@@ -54,53 +49,3 @@ function t_all (co)
 	
 	return result
 end
-
-print("\nAnother set of tests!")
-local print = print
-local pairs = pairs
-local t_all = t_all
-function foo (_ENV, a)
-	print(a + b)
-	print("This should be your answer: ", _ENV.b)
-	local r = t_all()
-	for k,v in pairs(r.locals) do
-		print(k,v) 
-	end
-	print(r.globals.b)
-end  -- _ENV is local, so to find such an _ENV, you need to scan for local variables.
-
-
-foo({b = 14}, 12)
-
---print("\nEnd those tests")
-
-function test ()
-	local x = 1
-	local y = 2
-	local z = 3
-
-	local r = t_all()
-	print(r.globals.MESSAGE)
-end
-
-function second_trip ()
-	local h = "baytown"
-	
-	coroutine.yield()
-end
-
-function travel ()
-	local x = 0
-	local w = nice
-	second_trip ()
-end
-
-test()
-co = coroutine.create(travel)
-
-coroutine.resume(co)
-
-local s = t_all(co)
-
-print(s.locals[1].h)
-print(s.upvalues[2].nice)
