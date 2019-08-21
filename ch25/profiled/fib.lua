2ddf93255e7345eb6eaab91caaf83b2cdf8ce5f7
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2019-08-20 23:01:35 -0400

    Work

--- ch25/profiled/fib.lua
+++ ch25/profiled/fib.lua
@@ -1,25 +1,8 @@
---[[
-	Profile 'fib'.
-	
-	Note that the dummy function 'recurred' sends information to the
-profiler about how many calls to 'fib' are non-terminal!
-
-tbc -- non-terminal counter still is at zero?
-see if you can prove that memoization brings the counts down :)
-]]
-
-local non_terminal = 0
-
-local function recurred () non_terminal  = non_terminal + 1 end
-
-
-local function fib (n)
+function fib (n)
 	if n == 0 then return 0 end
 	if n == 1 then return 1 end
 	
-	recurred()
 	return fib(n - 1) + fib(n - 2)
 end
 
-print("non terminal:", non_terminal)
 fib(3)
